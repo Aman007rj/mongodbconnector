7531c93ee10da6dbb02fe2fc9eb16840834f9769
Aman007rj <aman007rj@gamil.com>
2024-02-18 00:06:48 +0530

    code updated

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+__version__ = "0.0.1"
+REPO_NAME = "mongodbconnector"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "Aman007rj"
+AUTHOR_EMAIL = "aman007rj@gmail.com"
 
 setup(
     name=PKG_NAME,
@@ -25,4 +40,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=get_requirements('requirements_dev.txt')
     )
